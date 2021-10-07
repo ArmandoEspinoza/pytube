1123035842fa7c645e8a9e91adfb333c8d380f38
Mario Robles <mario.robles@digitalonus.com>
2021-10-07 18:40:05 -0500

    app added

---
+++ pytube.py
@@ -0,0 +1,33 @@
+#For this exercise I use a library called "Pytube" that bring us the possibility to download videos:
+
+# importing pytube
+from pytube import YouTube
+
+# here's where the video is going to be saved
+SAVE_PATH = "/Users/mariorobles/Desktop" #you can customize the path
+
+#just a welcome
+print("Hi! welcome to the python youtube video downloader! ヽ(・∀・)ﾉ")
+
+# link of the video 
+link=input("Please paste the link of the video you want to download!ᕕ( ᐛ )ᕗ : \n")
+yt = YouTube(link)
+
+#Showing details
+print("Your video details! (〜￣▽￣)〜  : ")
+print("Title: ",yt.title)
+print("Number of views: ",yt.views)
+print("Length of video: ",yt.length)
+print("Rating of video: ",yt.rating)
+
+#Just feedback for the user
+print("Downloading! (¬‿¬ ")
+
+#here the object is created using YouTube
+video = yt.streams.get_highest_resolution()
+
+
+# downloading the video
+video.download(SAVE_PATH)
+
+print('Your video was downladed! ヾ(☆▽☆)')
